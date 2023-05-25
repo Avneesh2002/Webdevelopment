a0ec7c5ae50872996a21cf73cbb2663bb7ebbf9b
Avneesh Kumar <avneeshdagur2001@gmil.com>
2023-05-25 09:42:51 +0530

    My first Commit

---
+++ CB/Grid/Javascript/function.js
@@ -0,0 +1,17 @@
+var a=10;
+
+function add(a,b) {
+    // return a+b;
+    var y =20;
+    console.log('Inside add funcrion');
+    console.log(y);
+    var y=20;
+    
+}
+add();
+
+console.log(a);
+function c(){
+    console.log()
+}
+// console.log(add(5,7));
---
+++ CB/Grid/Javascript/lect1.js
@@ -0,0 +1,11 @@
+var person={
+    Name:"aksdjf",
+    age: 25,
+    isadult: true,
+    marks:[98,40,68]
+}
+console.log(person.Name);
+console.log(person.age);
+console.log(person.isadult);
+console.log(person.marks);
+console.log(person.marks[1]);
